281c641f59beff1e75d8e44b2d5946a46d1c887c
asycodes <asyrafomar68@gmail.com>
2024-07-01 01:46:23 +0800

    added dcheck

--- source/system_programs/dcheck.c
+++ source/system_programs/dcheck.c
@@ -1,5 +1,36 @@
 #include "system_program.h"
 
+
 int main() {
+    int status;
+    char buffer[256];
+    FILE *file;
+    int count = 0;
+    const char *term = "dspawn";
+    //printf("dcheck running...\n");
+
+    // system function allows to make shell commands
+    status = system("ps -ef > output_dcheck.txt");
+    if (status == -1) {
+        perror("system");
+        return 1;
+    } else {
+        file = fopen("output_dcheck.txt","r");
+        if (file == NULL){
+            perror("File open failed");
+            return 1;
+        }
+        while(fgets(buffer,sizeof(buffer),file) != NULL){
+            // we check line by line !
+            if(strstr(buffer,term) != NULL){
+                count++;
+            }
+        }
+        fclose(file);
+        printf("Live daemon count: %d\n", count);
+    }
+
+    //printf("dcheck completed.\n");
+
     return 0;
 }
